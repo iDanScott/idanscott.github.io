3012f1d5a21944058af410544f9a1b93c106bbae
Dan Scott <tehdanscott@gmail.com>
2018-07-31 23:56:56 +0100

    few cosmetic changes

--- chaplin-windows/Scripts/main.js
+++ chaplin-windows/Scripts/main.js
@@ -6,15 +6,7 @@ $(function () {
 
 $(document).scroll(function () {
     var $nav = $("nav.fixed-top");
-    var $brand = $("<div class='col-lg'><a class='brand' href='#'><img style='height:50px' src='./Images/double-glazing-alton-hampshire-chaplin-windows-logo.png'></a></div>");
     $nav.toggleClass('scrolled', $(this).scrollTop() > 0, 1000);
     $nav.toggleClass('bg-light', $(this).scrollTop() > 0, 1000);
     $nav.toggleClass('bg-transparent', $(this).scrollTop() == 0, 1000);
-    if ($(this).scrollTop() == 0) {
-        $('.navbar-nav').find('.col-lg').has('.brand').remove();
-        appendedBrand= false;
-    } else if (!appendedBrand) {
-        $('.navbar-nav').prepend($brand);
-        appendedBrand = true;
-    }
 });
