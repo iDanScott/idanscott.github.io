5091991c75cc87198df1bbe36f7772a52eaf75ba
Dan Scott <tehdanscott@gmail.com>
2018-07-31 23:04:04 +0100

    Chaplin Windows

---
+++ chaplin-windows/Scripts/main.js
@@ -0,0 +1,12 @@
+﻿$(function () {
+    $('body').scrollspy({ target: '#navbar-main' });
+    $('.carousel').carousel();
+});
+
+$(document).scroll(function () {
+    var $nav = $("nav.fixed-top");
+    $nav.animate();
+    $nav.toggleClass('scrolled', $(this).scrollTop() > 0, 1000);
+    $nav.toggleClass('bg-light', $(this).scrollTop() > 0, 1000);
+    $nav.toggleClass('bg-transparent', $(this).scrollTop() == 0, 1000);
+});
